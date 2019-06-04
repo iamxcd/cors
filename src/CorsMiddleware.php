a4cff945c09f690ae87ae53dbd6fcde201c8eb3b
xiansongbai <songbai_x@qq.com>
2019-06-04 21:51:28 +0800

    [fix] 增加DELETE

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -14,7 +14,7 @@ class  CorsMiddleware
         $response->header('Access-Control-Allow-Origin', "*");
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
         $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
 
         return $response;
